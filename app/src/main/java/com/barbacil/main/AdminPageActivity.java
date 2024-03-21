5e3d7f154465ce38de7b4a860edeb8d75a22efb8
Shaekhrom <shaekhrom@gmail.com>
2024-03-22 00:55:15 +0100

    early0.4

---
+++ app/src/main/java/com/barbacil/main/AdminPageActivity.java
@@ -0,0 +1,14 @@
+package com.barbacil.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_page);
+    }
+}
---
+++ app/src/main/java/com/barbacil/main/CompanyCreatorActivity.java
@@ -0,0 +1,14 @@
+package com.barbacil.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompanyCreatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_creator);
+    }
+}
